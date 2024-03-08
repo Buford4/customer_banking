24fcaa152bfc33a162f049558d310c700bdfa4b9
Buford4 <rbbdun4@gmail.com>
2024-03-07 18:56:52 -0700

    Commit it

--- customer_banking.py
+++ customer_banking.py
@@ -13,29 +13,39 @@ def main():
     # ADD YOUR CODE HERE
     savings_balance = float(input("Enter the savings balance: "))
     savings_interest = float(input("Enter the savings interest rate: "))
-    savings_maturity = int(input("Enter the savings maturity in months: "))
+    savings_maturity = input("Enter the savings maturity in months: ")
+    if savings_maturity.isdigit():
+        savings_maturity = int(savings_maturity)
+    else:
+        print("Your savings maturity input is not a number. Please enter a number.")
+        exit() 
 
     # Call the create_savings_account function and pass the variables from the user.
-    updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
+    updated_savings_balance, savings_interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Interest earned on savings: {interest_earned}")
-    print(f"Updated savings balance: {updated_savings_balance}")
+    print(f"Interest earned: ${savings_interest_earned:,.2f}")
+    print(f"Updated savings account balance: ${updated_savings_balance:,.2f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
-    cd_balance = float(input("Enter the CD balance: "))
-    cd_interest = float(input("Enter the CD interest rate: "))
-    cd_maturity = int(input("Enter the CD maturity in months: "))
+    cd_balance = float (input("Please enter CD balance:$"))
+    cd_interest= float(input("Please enter CD interest rate:"))
+    cd_maturity = input("Please enter number of months: ")
+    if cd_maturity.isdigit():
+        cd_maturity = int(cd_maturity)
+    else:
+        print("Your cd maturity input is invalid")
+        exit()
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    updated_cd_balance, cd_interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
-    print(f"Interest earned on CD: {interest_earned}")
-    print(f"Updated CD balance: {updated_cd_balance}")
+    print(f"Interest earned: ${cd_interest_earned:,.2f}")
+    print(f"Updated cd account balance: ${updated_cd_balance:,.2f}")
 
 if __name__ == "__main__":
     # Call the main function.
--- savings_account.py
+++ savings_account.py
@@ -2,19 +2,9 @@
 # ADD YOUR CODE HERE
 from Account import Account
 
-def get_user_input():           
-    while True:
-        user_input = input("Enter a value: ")
-        if user_input:
-            return user_input
-        else:
-            print("Please enter a value: ")
-
-user_input = get_user_input()
-print("You entered:", user_input)
-
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
+
     """Creates a savings account, calculates interest earned, and updates the account balance.
 
     Args:
@@ -29,25 +19,23 @@ def create_savings_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
-    account_instance = Account(balance, interest_rate)
+    account = Account(balance, 0)
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
-    interest_earned = balance * (interest_rate / 100 ) * (months /12 )
-    print(interest_earned)
+    savings_interest_earned = balance * (interest_rate/100 * months/12)
 
     # Update the savings account balance by adding the interest earned
     # ADD YOUR CODE HERE
-    updated_balance = balance + interest_earned
+    updated_savings_balance = balance + savings_interest_earned
 
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
-    account_instance.set_balance(updated_balance)
+    account.set_balance(updated_savings_balance)
 
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
     # ADD YOUR CODE HERE
-    account_instance.set_interest(interest_earned)
-
+    account.set_interest(savings_interest_earned)
 
     # Return the updated balance and interest earned.
-    return updated_balance, interest_earned
+    return  updated_savings_balance, savings_interest_earned
